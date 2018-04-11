4bbfa8b3e3446de0e7788bd497b39d247da2fc85
rupsam <37034880+rupsam@users.noreply.github.com>
2018-04-11 13:19:33 +0000

    Update TestFileHello.java

--- TestFileHello.java
+++ TestFileHello.java
@@ -1,6 +1,6 @@
 public class TestFileHello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window aws.
-      System.out.println("Hello, World9");
+      System.out.println("Hello, World939");
    }
 }
